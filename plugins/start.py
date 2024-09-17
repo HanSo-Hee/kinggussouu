cd1143f4eb0d920c2dceb5f32ca39fbb9b2369c9
Debjani Das <131546223+HanSo-Hee@users.noreply.github.com>
2024-09-17 18:25:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,6 @@ from database.database import add_user, del_user, full_userbase, present_user
 
 
 
-
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
@@ -27,7 +26,7 @@ async def start_command(client: Client, message: Message):
         except:
             pass
     text = message.text
-    if len(text)>7:
+    if len(text) > 7:
         try:
             base64_string = text.split(" ", 1)[1]
         except:
@@ -41,7 +40,7 @@ async def start_command(client: Client, message: Message):
             except:
                 return
             if start <= end:
-                ids = range(start,end+1)
+                ids = range(start, end + 1)
             else:
                 ids = []
                 i = start
@@ -63,10 +62,12 @@ async def start_command(client: Client, message: Message):
             return
         await temp_msg.delete()
 
-        for msg in messages:
+        batch_messages = []  # Store messages sent in the batch
 
+        for msg in messages:
             if bool(CUSTOM_CAPTION) & bool(msg.document):
-                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
+                caption = CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html,
+                                                filename=msg.document.file_name)
             else:
                 caption = "" if not msg.caption else msg.caption.html
 
@@ -76,36 +77,52 @@ async def start_command(client: Client, message: Message):
                 reply_markup = None
 
             try:
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                sent_msg = await msg.copy(chat_id=message.from_user.id, caption = f"{caption}\n\n📁 File Will Auto Delete In ⏰ 30 Minutes.↗️ Forward It Anywhere Or Save It Privately Before Downloading", parse_mode=ParseMode.HTML,
+                                          reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
+                batch_messages.append(sent_msg)
                 await asyncio.sleep(0.5)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                sent_msg = await msg.copy(chat_id=message.from_user.id, caption = f"{caption}\n\n📁 File Will Auto Delete In ⏰ 30 Minutes.↗️ Forward It Anywhere Or Save It Privately Before Downloading", parse_mode=ParseMode.HTML,
+                                          reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
+                batch_messages.append(sent_msg)
             except:
                 pass
+
+        asyncio.create_task(delete_batch_messages(batch_messages))
+
         return
     else:
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("🤖 Channel", url="https://t.me/KS_Team_Live"),
+                    InlineKeyboardButton("🔒 Close", callback_data="close")
                 ]
             ]
         )
         await message.reply_text(
-            text = START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
+            text=START_MSG.format(
+                first=message.from_user.first_name,
+                last=message.from_user.last_name,
+                username=None if not message.from_user.username else '@' + message.from_user.username,
+                mention=message.from_user.mention,
+                id=message.from_user.id
             ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
+            reply_markup=reply_markup,
+            disable_web_page_preview=True,
+            quote=True
         )
         return
+
+
+async def delete_batch_messages(batch_messages):
+    await asyncio.sleep(30 * 60)  # 30 minutes delay for deletion
+    for msg in batch_messages:
+        try:
+            await msg.delete()
+        except:
+            pass
 
     
 #=====================================================================================##
